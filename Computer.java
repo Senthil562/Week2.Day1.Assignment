b48497e20b650f11ade77c42cf60ae82f2d16846
Senthil562 <88609292+Senthil562@users.noreply.github.com>
2021-08-25 22:58:26 +0530

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,10 @@
+package org.system;
+
+import java.awt.Desktop;
+
+public class Computer {
+	public void computerModel() {
+		// TODO Auto-generated method stub
+		System.out.println("The Model of the computer is HP-15q1234NP");
+	}
+}
